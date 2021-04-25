d1c828107b962bc213d41852f347423b5436c776
Nick Reese <nwreese@kent.edu>
2021-04-25 17:40:29 -0400

    adding components and working on pages

--- models/book.js
+++ models/book.js
@@ -4,7 +4,6 @@ const Schema = mongoose.Schema;
 const bookSchema = new Schema({
   title: { type: String, required: true },
   authors: { type: String, required: true },
-  synopsis: String,
   description: {type: String, required: true },
   image: {data: Buffer, contentType: String},
   link: {type: String, required: true }
